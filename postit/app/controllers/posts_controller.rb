f862e1ff39ea0a3c223550c9792546f118a70cf2
floalex <flo.liang@my.com>
2017-05-26 06:50:17 +0000

    Add Edit Post

--- postit/app/controllers/posts_controller.rb
+++ postit/app/controllers/posts_controller.rb
@@ -19,14 +19,23 @@ class PostsController < ApplicationController
       flash[:notice] = 'New post has been created.'
       redirect_to posts_path
     else
-      render 'new'
+      render :new
     end
   end
   
   def edit
+    @post = Post.find(params[:id])
   end
   
   def update
+    @post = Post.find(params[:id])
+    
+    if @post.update(post_params)
+      flash[:notice] = 'The post has been updated.'
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
   
   private 
